785026a0026ae2ecbed2fd483593d52b37a351d3
Dominic <Domnick080@outlook.com>
2024-09-03 09:38:32 +0100

    adjust ui (#12)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import ContactUs from "@/components/ContactUs";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
-import Services from '@/components/Services'
+import Services from "@/components/Services";
 import LandingPage from "@/wrappers/LandingPage";
-
 
 export default function Home() {
   return (
     <LandingPage>
-    <Hero/>
-    <Services/>
-    <Projects/>
-    <ContactUs/>
+      <Hero />
+      <Services />
+      <Projects />
+      <ContactUs />
     </LandingPage>
   );
 }
