16fe6fc17688d5025d955d0d94524ce5f9aa6b47
Abdulsobur <aabdulsobur46@gmail.com>
2024-09-10 14:58:18 +0100

    chore: set up the env file, also update teh about us content.

--- src/app/about-us/content.tsx
+++ src/app/about-us/content.tsx
@@ -2,69 +2,132 @@ import { Box, Button, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 
+const solutions = [
+  {
+    title: "Extensive Talent Database",
+    desc: "Access a curated pool of qualified developers and designers tailored to your project needs.",
+  },
+  {
+    title: "Rigorous Screening and Vetting",
+    desc: "Our candidates undergo a thorough evaluation to ensure top-quality results.",
+  },
+  {
+    title: "Rigorous Screening and Vetting",
+    desc: "Our candidates undergo a thorough evaluation to ensure top-quality results.",
+  },
+  {
+    title: "Expert Digital Teams",
+    desc: " Beyond freelancers, our in-house professionals deliver end-to-end project execution.",
+  },
+  {
+    title: "Customized Recruitment",
+    desc: " Work with our expert recruiters to find specialists that align with your goals, timelines, and budget.",
+  },
+];
+const supports = [
+  {
+    title: "Exclusive Job Board",
+    desc: "Discover entry-level roles and internships to kickstart your career.",
+  },
+  {
+    title: "Vibrant Community",
+    desc: " Join our social media platforms for networking, learning, and mentorship.",
+  },
+  {
+    title: "Career Guidance",
+    desc: "Access tips and resources for crafting standout resumes, mastering interviews, and navigating the tech industry.",
+  },
+];
+const services = [
+  {
+    title: "UI/UX Design and Web Development",
+    desc: "Create visually stunning and user-friendly websites and apps.",
+  },
+  {
+    title: "Graphic Design",
+    desc: "Develop compelling visuals, from logos to marketing materials.",
+  },
+  {
+    title: "Graphic Design",
+    desc: "Develop compelling visuals, from logos to marketing materials.",
+  },
+  {
+    title: "Software Development",
+    desc: "Build custom software solutions with our dedicated team and freelance talent.",
+  },
+  {
+    title: "Digital Marketing",
+    desc: "Implement data-driven strategies in SEO, social media, and content creation.",
+  },
+];
+
 export default function Content() {
   return (
     <Stack spacing={2}>
       <Typography>
-        Codmify is a platform that connects businesses with expert developers
-        and designers, and helps junior designers and techies find jobs and get
-        hired. We are committed to providing our clients with the best possible
-        service, and we make it our mission to help junior techies reach their
-        full potential.
+        At Codmify, we’re more than just a talent marketplace—we are your
+        comprehensive digital solutions partner. Our platform seamlessly
+        connects businesses with top-tier developers, designers, and a network
+        of skilled professionals ready to bring your projects to life. From
+        software development to digital marketing, we have the expertise to
+        drive your success.
       </Typography>
-      <Typography>
-        We understand that finding the right talent can be a challenge,
-        especially when it comes to specialized fields like development and
-        design. That&lsquo;s why we offer a variety of services to help
-        businesses find the perfect candidates for their projects.
-      </Typography>
-      <Box>
-        <Typography>Our services include:</Typography>
-        <Box component={"ul"} pl={3}>
-          <Typography component={"li"}>
-            A large database of qualified developers and designers
+      <Stack gap="30px">
+        <Box>
+          <Typography>
+            Navigating the talent landscape can be daunting. Codmify simplifies
+            this process by offering:
           </Typography>
-          <Typography component={"li"}>
-            A screening process to ensure that all candidates meet our high
-            standards
-          </Typography>
-          <Typography component={"li"}>
-            A team of experienced recruiters who can help you find the right
-            talent for your specific needs
-          </Typography>
+          <Box component={"ul"} pl={3}>
+            {solutions.map((cnt) => (
+              <Typography component={"li"} key={cnt.title} sx={{ mt: "12px" }}>
+                <span style={{ fontWeight: 600 }}>{`${cnt.title}:`}</span>
+                {cnt.desc}
+              </Typography>
+            ))}
+          </Box>
         </Box>
-      </Box>
-      <Box>
-        <Typography>
-          We also offer a variety of resources to help junior designers and
-          techies find jobs and get hired. These resources include:
-        </Typography>
-        <Box component={"ul"} pl={3}>
-          <Typography component={"li"}>
-            A job board with listings for entry-level positions and internships
+        <Box>
+          <Typography>
+            We are committed to fostering the growth of junior talent. Our
+            support includes:
           </Typography>
-          <Typography component={"li"}>
-            A community on WhatsApp where junior designers and techies can
-            connect with each other and learn from experienced developers and
-            designers
-          </Typography>
-          <Typography component={"li"}>
-            A variety of articles and blog posts with tips on how to find a job
-            in tech, write a resume, and prepare for an interview
-          </Typography>
+          <Box component={"ul"} pl={3}>
+            {supports.map((cnt) => (
+              <Typography component={"li"} key={cnt.title} sx={{ mt: "12px" }}>
+                <span style={{ fontWeight: 600 }}>{`${cnt.title}:`}</span>
+                {cnt.desc}
+              </Typography>
+            ))}
+          </Box>
         </Box>
+        <Box>
+          <Typography>
+            Codmify also offers direct access to a range of digital services
+            through our in-house experts:
+          </Typography>
+          <Box component={"ul"} pl={3}>
+            {services.map((cnt) => (
+              <Typography component={"li"} key={cnt.title} sx={{ mt: "12px" }}>
+                <span style={{ fontWeight: 600 }}>{`${cnt.title}:`}</span>
+                {cnt.desc}
+              </Typography>
+            ))}
+          </Box>
+        </Box>
+      </Stack>
+      <Box mt={'30px'}>
+      <Typography>
+        Codmify is designed to be your all-in-one solution for both talent and
+        digital services. With a commitment to quality and innovation, we’re
+        here to help your business thrive while supporting the next generation
+        of digital talent.
+      </Typography>
+      <Typography mt="15px">
+        Ready to elevate your business? Partner with Codmify and explore our
+        comprehensive digital solutions.
+      </Typography>
       </Box>
-      <Typography>
-        At Codmify, we believe that everyone has the potential to be a great
-        developer or designer. We are committed to helping our clients find the
-        best possible talent, and we are dedicated to helping junior designers
-        and techies reach their full potential.
-      </Typography>
-      <Typography>
-        If you are a business looking for expert developers or designers, or if
-        you are a junior designer or techie looking for a job, contact Codmify
-        today!
-      </Typography>
       <Box py={2} />
       <Button
         size="large"
