98873ea9a3acb3d81f1a8c76a95e621c6cfb6cde
decryptus007 <domnick080@outlook.com>
2024-09-27 19:05:18 +0100

    push whatsapp chat icon

--- src/wrappers/LandingPage.tsx
+++ src/wrappers/LandingPage.tsx
@@ -2,16 +2,18 @@
 
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import WhatsappChat from "@/components/Whatsapp";
 import { Box } from "@mui/material";
 import React, { ReactNode } from "react";
 
 export default function LandingPage({ children }: { children: ReactNode }) {
   return (
-    <Box position={"relative"}>
+    <Box position={"relative"} sx={{ width: "100%" }}>
       <Navbar />
       <Box component={"main"} minHeight={"90dvh"}>
         {children}
       </Box>
+      <WhatsappChat />
       <Footer />
     </Box>
   );
